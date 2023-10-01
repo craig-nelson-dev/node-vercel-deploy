79f30e92ef070ad412c572f8f0f15d93431f81e1
gracephilipse <gracephilipse@outlook.com>
2023-10-01 00:02:13 -0400

    mongodb version updated

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-
 // Import required modules
 const express = require('express');
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
 
-// // Create a MongoDB schema for User
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: String,
-  cover_letter: String
-});
-
-// Create a User model based on the schema
-const User = mongoose.model('User', userSchema);
-
-// // Connect to MongoDB
-mongoose.connect('mongodb://0.0.0.0:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-
-// // Create an Express app
+// Create an Express app
 const app = express();
-app.use(bodyParser.json());
-app.use(cors())
+app.use(bodyParser.json(), cors());
 
-// // Generate a token
+// Generate a token
 function generateToken(user) {
   return jwt.sign({ id: user._id, role: user.role, username: user.username }, 'your-secret-key', { expiresIn: '1h' });
 }
 
-// // Middleware to verify token
+// Middleware to verify token
 function verifyToken(req, res, next) {
   const token = req.headers.authorization;
   if (!token) {
@@ -52,69 +30,81 @@ function verifyToken(req, res, next) {
   });
 }
 
-// // API endpoints
+// Connect to MongoDB
+const uri = 'mongodb://0.0.0.0:27017';
+const client = new MongoClient(uri);
+let db;
 
-// app.post('/users', verifyToken, async (req, res) => {
-//   try {
-//     if (req.user.role !== 'admin') {
-//       return res.status(403).json({ error: 'Forbidden' });
-//     }
+console.log()
 
-//     const { username, password, role, cover_letter } = req.body;
-    
-//     // Check if username already exists
-//     const existingUser = await User.findOne({ username });
-//     if (existingUser) {
-//       return res.status(400).json({ error: 'Username already exists' });
-//     }
+client.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
+  console.log('Connected to MongoDB');
+  db = client.db('mydatabase');
+});
 
-//     const user = new User({ username, password, role, cover_letter });
-//     await user.save();
-//     res.status(201).json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to create user' });
-//   }
-// });
+// API endpoints
 
+app.post('/users', verifyToken, async (req, res) => {
+  try {
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
 
-// app.post('/verify-password', async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     const user = await User.findOne({ username, password });
-//     if (user) {
-//       const token = generateToken(user);
-//       res.status(200).json({ token });
-//     } else {
-//       res.status(401).json({ error: 'Invalid username or password' });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to verify password' });
-//   }
-// });
+    const { username, password, role, cover_letter } = req.body;
 
-// app.get('/cover_letter',  async (req, res) => {
-//   try {
-//     // const { username } = req.params;
-//     // if (req.user.role === 'admin' || req.user.username !== username ) {
-//     //   res.status(403).json({ error: 'Forbidden' });
-//     // } else {
-//     //   res.json("hello")
-//     // }
+    // Check if username already exists
+    const existingUser = await db.collection('users').findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
 
-//     res.json("hallo")
+    const user = { username, password, role, cover_letter };
+    await db.collection('users').insertOne(user);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+});
 
+app.post('/verify-password', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await db.collection('users').findOne({ username, password });
+    if (user) {
+      const token = generateToken(user);
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ error: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Failed to verify password' });
+  }
+});
 
-//     // const user = await User.findOne({ username });
-//     // if (user) {
-//     //   res.status(200).json({ cover_letter: user.cover_letter });
-//     // } else {
-//     //   res.status(404).json({ error: 'User not found' });
-//     // }
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to retrieve cover letter' });
-//   }
-// });
+app.get('/cover-letter/:username', verifyToken, async (req, res) => {
+  try {
+    const { username } = req.params;
+    if (req.user.role === 'admin' || req.user.username !== username) {
+      res.status(403).json({ error: 'Forbidden' });
+    } else {
+      res.json("hello");
+    }
 
+    const user = await db.collection('users').findOne({ username });
+    if (user) {
+      res.status(200).json({ cover_letter: user.cover_letter });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve cover letter' });
+  }
+});
 
 app.get('/', async (req, res) => {
   try {
@@ -125,6 +115,6 @@ app.get('/', async (req, res) => {
 });
 
 // Start the server
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
