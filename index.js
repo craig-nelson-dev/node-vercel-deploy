36dc58b362a19ec44dc76df54f52714e9b763727
gracephilipse <gracephilipse@outlook.com>
2023-09-30 22:19:07 -0400

    npm init again

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://0.0.0.0:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
